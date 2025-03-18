c5533b05a46368dcb1f2d9710eac31a7c65f39e0
Gabriel Alexandratos <gabriel.alexandratos@education.nsw.gov.au>
2025-03-18 11:58:41 +1100

    passed einstein.py test

---
+++ src/function_variables/einstein.py
@@ -0,0 +1,8 @@
+def einstein():
+    input_mass = input()
+    int_input_mass = int(input_mass)
+    energy = int_input_mass * 300000000 ** 2
+    print(energy)
+
+if __name__ == "__main__":
+    einstein()
